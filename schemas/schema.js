bbf2e1b632581d5fbfb541a90977e677334ddf73
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-08-08 15:50:59 +0900

    블로그 컨텐트 블록 내에서 링크 외부링크로 삽입되도록 스키마 수정

--- schemas/schema.js
+++ schemas/schema.js
@@ -53,6 +53,28 @@ export default createSchema({
 					of: [
 						{
 							type: 'block',
+							marks: {
+								annotations: [
+									{
+										name: 'link',
+										type: 'object',
+										title: 'Link',
+										fields: [
+											{
+												name: 'href',
+												type: 'url',
+												title: 'URL',
+											},
+											{
+												title: 'Open in new tab',
+												name: 'blank',
+												description: 'Read https://css-tricks.com/use-target_blank/',
+												type: 'boolean',
+											},
+										],
+									},
+								],
+							},
 						},
 						{
 							type: 'image',
